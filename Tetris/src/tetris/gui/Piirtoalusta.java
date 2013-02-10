c9c2d6e254a9765a5a906ff845c731ff1311f4ce
Tommi Nikkanen <tommi.nikkanen@cs.helsinki.fi>
2013-02-11 01:38:02 +0200

    Tuntikirjanpito päivitetty

--- Tetris/src/tetris/gui/Piirtoalusta.java
+++ Tetris/src/tetris/gui/Piirtoalusta.java
@@ -1,13 +1,17 @@
 package tetris.gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import tetris.domain.Muoto;
 import tetris.domain.Pala;
 import tetris.peli.Peli;
+
 
 /**
  * Piirtoalusta tetrikselle
@@ -18,17 +22,20 @@ public class Piirtoalusta extends JPanel implements Paivitettava {
 
     private Peli tetrisPeli;
     Image taustakuva;
-    Image pala_purppura;
+    Font pisteFontti;
 
     public Piirtoalusta(Peli tetrisPeli) {
         this.tetrisPeli = tetrisPeli;
         taustakuva = new ImageIcon(this.getClass().getResource("../bg.jpg")).getImage();
+        pisteFontti = new Font("Helvetica", Font.PLAIN, 17);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        
+        Graphics2D g2d = (Graphics2D) g;
+        
         g.drawImage(taustakuva, 0, 0, this);
 
         for (Muoto muoto : this.tetrisPeli.getKaikkiMuodot()) {
@@ -37,8 +44,10 @@ public class Piirtoalusta extends JPanel implements Paivitettava {
                 g.drawImage(pala.getKuva(), pala.getX(), pala.getY(), null);
             }
         }
-        g.setColor(Color.GRAY);
-        g.drawString(tetrisPeli.getPistelaskuri().toString(), 10, 20);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2d.setFont(pisteFontti);
+        g2d.setColor(Color.GRAY);
+        g2d.drawString(tetrisPeli.getPistelaskuri().toString(), 10, 30);
 
     }
 
